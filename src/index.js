2459a5a0c5e75f7a035be95747de92c4a92843ad
shubhangi1922 <kumbharshubhangi13@gmail.com>
2024-01-20 22:40:09 +0530

    Complete Code of Application

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import store from "./store";
+import useAuthentication from "./hooks/useAuthentication";
+import useServices from "./hooks/useServices";
+
+const ConnectedApp = () => {
+	const {AuthProvider} = useAuthentication();
+	const {ServicesProvider} = useServices();
+	return (
+		<AuthProvider>
+			<ServicesProvider>
+				<Provider store={store}>
+					<App />
+				</Provider>
+			</ServicesProvider>
+		</AuthProvider>
+	);
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<ConnectedApp />
 );
 
 // If you want to start measuring performance in your app, pass a function
